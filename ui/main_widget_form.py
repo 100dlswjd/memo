fe6a2edeb93ac2f7ca1d3042df8f9d989263338b
100dlswjd <100dlswjd@naver.com>
2023-10-21 22:04:54 +0900

    메인 위젯 수정

--- ui/main_widget_form.py
+++ ui/main_widget_form.py
@@ -17,20 +17,31 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QFrame, QHBoxLayout,
     QLabel, QPushButton, QSizePolicy, QSpacerItem,
-    QTextEdit, QVBoxLayout, QWidget)
+    QTabWidget, QVBoxLayout, QWidget)
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(600, 400)
-        Form.setMinimumSize(QSize(600, 400))
-        Form.setStyleSheet(u"")
-        self.pushButton_show = QPushButton(Form)
+class Ui_MainWidget(object):
+    def setupUi(self, MainWidget):
+        if not MainWidget.objectName():
+            MainWidget.setObjectName(u"MainWidget")
+        MainWidget.resize(600, 400)
+        MainWidget.setMinimumSize(QSize(600, 400))
+        MainWidget.setStyleSheet(u"")
+        self.pushButton_show = QPushButton(MainWidget)
         self.pushButton_show.setObjectName(u"pushButton_show")
         self.pushButton_show.setGeometry(QRect(520, 10, 70, 80))
-        self.pushButton_show.setStyleSheet(u"")
-        self.label_move = QLabel(Form)
+        self.pushButton_show.setStyleSheet(u"QPushButton{\n"
+"border:3px solid #ffffff;\n"
+"border-radius:20px;\n"
+"padding:10px;\n"
+"background-color:#00da3e;\n"
+"}\n"
+"QPushButton:hover{\n"
+"background-color:#00c538;\n"
+"}\n"
+"QPushButton:Pressed{\n"
+"background-color:#00a72f;\n"
+"}")
+        self.label_move = QLabel(MainWidget)
         self.label_move.setObjectName(u"label_move")
         self.label_move.setGeometry(QRect(520, 80, 70, 22))
         self.label_move.setStyleSheet(u"QLabel{\n"
@@ -38,36 +49,61 @@ class Ui_Form(object):
 "border-radius:8px;\n"
 "background-color:qlineargradient(spread:pad, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(0, 218, 62, 255), stop:1 rgba(255, 255, 255, 255));\n"
 "}")
-        self.frame = QFrame(Form)
-        self.frame.setObjectName(u"frame")
-        self.frame.setEnabled(True)
-        self.frame.setGeometry(QRect(520, 10, 40, 40))
-        self.frame.setMinimumSize(QSize(0, 0))
-        self.frame.setStyleSheet(u"QFrame{\n"
+        self.frame_main = QFrame(MainWidget)
+        self.frame_main.setObjectName(u"frame_main")
+        self.frame_main.setEnabled(True)
+        self.frame_main.setGeometry(QRect(520, 10, 40, 40))
+        self.frame_main.setMinimumSize(QSize(0, 0))
+        self.frame_main.setStyleSheet(u"QFrame{\n"
 "background-color:qlineargradient(spread:pad, x1:1, y1:0, x2:0, y2:1, stop:0 rgba(0, 237, 69, 255), stop:1 rgba(255, 255, 255, 255));\n"
 "border:3px solid #ffffff;\n"
 "border-radius:20px;\n"
 "}")
-        self.verticalLayout_2 = QVBoxLayout(self.frame)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_main)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(4, 4, 4, 4)
-        self.textEdit_memo = QTextEdit(self.frame)
-        self.textEdit_memo.setObjectName(u"textEdit_memo")
-        self.textEdit_memo.setStyleSheet(u"QTextEdit{\n"
-"border:1px solid #00da3e;\n"
-"background-color:#ffffff;\n"
-"padding:10px;\n"
+        self.tabWidget_memo = QTabWidget(self.frame_main)
+        self.tabWidget_memo.setObjectName(u"tabWidget_memo")
+        self.tabWidget_memo.setStyleSheet(u"QTabBar:tab{\n"
+"border:2px solid #ffffff;\n"
+"border-radius:10px;\n"
+"width:80px;\n"
+"height:20px;\n"
+"background-color:#47f279;\n"
+"}\n"
+"QTabBar:tab:hover{\n"
+"background-color:#3dd167;\n"
+"}\n"
+"QTabBar:tab:Pressed{\n"
+"background-color:#35b85a;\n"
+"}\n"
+"QTabWidget:pane {\n"
+"border:none;\n"
+"border-radius:none;\n"
+"padding:0px\n"
+"}\n"
+"QTabBar:tab:selected{\n"
+"background-color:#00a72f;\n"
+"color:#ffffff;\n"
+"}\n"
+"QTabBar:close-button{\n"
+"image:url(\"D:/git/100dlswjd/memo/img/close.png\");\n"
 "}")
 
-        self.verticalLayout.addWidget(self.textEdit_memo)
+        self.verticalLayout.addWidget(self.tabWidget_memo)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(4, 4, 4, 4)
-        self.label_error = QLabel(self.frame)
+        self.label_error = QLabel(self.frame_main)
         self.label_error.setObjectName(u"label_error")
+        sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_error.sizePolicy().hasHeightForWidth())
+        self.label_error.setSizePolicy(sizePolicy)
         self.label_error.setStyleSheet(u"QLabel{\n"
 "border:none;\n"
 "background-color:none;\n"
@@ -83,11 +119,29 @@ class Ui_Form(object):
 
         self.horizontalLayout.addItem(self.horizontalSpacer)
 
-        self.checkBox_auto = QCheckBox(self.frame)
+        self.checkBox_floating = QCheckBox(self.frame_main)
+        self.checkBox_floating.setObjectName(u"checkBox_floating")
+        sizePolicy.setHeightForWidth(self.checkBox_floating.sizePolicy().hasHeightForWidth())
+        self.checkBox_floating.setSizePolicy(sizePolicy)
+        self.checkBox_floating.setStyleSheet(u"QCheckBox{\n"
+"color:#409047;\n"
+"}\n"
+"QCheckBox:indicator{\n"
+"border:2px solid #72ff7e;\n"
+"border-radius:6px;\n"
+"}\n"
+"QCheckBox:indicator:checked{\n"
+"border:1px solid #72ff7e;\n"
+"padding:2px;\n"
+"background-color:#58c561;\n"
+"}")
+        self.checkBox_floating.setCheckable(True)
+        self.checkBox_floating.setChecked(False)
+
+        self.horizontalLayout.addWidget(self.checkBox_floating)
+
+        self.checkBox_auto = QCheckBox(self.frame_main)
         self.checkBox_auto.setObjectName(u"checkBox_auto")
-        sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.checkBox_auto.sizePolicy().hasHeightForWidth())
         self.checkBox_auto.setSizePolicy(sizePolicy)
         self.checkBox_auto.setStyleSheet(u"QCheckBox{\n"
@@ -113,26 +167,41 @@ class Ui_Form(object):
 
         self.verticalLayout_2.addLayout(self.verticalLayout)
 
-        self.pushButton_close = QPushButton(Form)
+        self.pushButton_close = QPushButton(MainWidget)
         self.pushButton_close.setObjectName(u"pushButton_close")
         self.pushButton_close.setGeometry(QRect(520, 110, 70, 40))
-        self.pushButton_close.setStyleSheet(u"")
-        self.frame.raise_()
+        self.pushButton_close.setStyleSheet(u"QPushButton{\n"
+"border:3px solid #ffffff;\n"
+"border-radius:20px;\n"
+"padding:10px;\n"
+"background-color:#00da3e;\n"
+"}\n"
+"QPushButton:hover{\n"
+"background-color:#00c538;\n"
+"}\n"
+"QPushButton:Pressed{\n"
+"background-color:#00a72f;\n"
+"}")
+        self.frame_main.raise_()
         self.pushButton_show.raise_()
         self.label_move.raise_()
         self.pushButton_close.raise_()
 
-        self.retranslateUi(Form)
+        self.retranslateUi(MainWidget)
 
-        QMetaObject.connectSlotsByName(Form)
+        self.tabWidget_memo.setCurrentIndex(-1)
+
+
+        QMetaObject.connectSlotsByName(MainWidget)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+    def retranslateUi(self, MainWidget):
+        MainWidget.setWindowTitle(QCoreApplication.translate("MainWidget", u"Form", None))
         self.pushButton_show.setText("")
         self.label_move.setText("")
-        self.label_error.setText(QCoreApplication.translate("Form", u"\ubc84\uadf8 \ubb38\uc758\ub294 \ub543\uc950\uc5d0\uac8c", None))
-        self.checkBox_auto.setText(QCoreApplication.translate("Form", u"\uc790\ub3d9 \ucd95\uc18c", None))
+        self.label_error.setText(QCoreApplication.translate("MainWidget", u"\ubc84\uadf8 \ubb38\uc758\ub294 \ub543\uc950\uc5d0\uac8c", None))
+        self.checkBox_floating.setText(QCoreApplication.translate("MainWidget", u"\ud56d\uc0c1 \uc704", None))
+        self.checkBox_auto.setText(QCoreApplication.translate("MainWidget", u"\uc790\ub3d9 \ucd95\uc18c", None))
         self.pushButton_close.setText("")
     # retranslateUi
 
